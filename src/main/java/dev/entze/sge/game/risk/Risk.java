640f3f9edfd1eb5e1e432c552f5df47c1b756905
Lukas Grassauer <lukas@grassauer.eu>
2019-04-20 14:40:17 +0200

    cleanup and restructure of how Risk is loaded

--- src/main/java/dev/entze/sge/game/risk/Risk.java
+++ src/main/java/dev/entze/sge/game/risk/Risk.java
@@ -2,6 +2,7 @@ package dev.entze.sge.game.risk;
 
 import dev.entze.sge.game.ActionRecord;
 import dev.entze.sge.game.Game;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -12,12 +13,35 @@ public class Risk implements Game<RiskAction, RiskBoard> {
   private List<ActionRecord<RiskAction>> actionRecords;
   private RiskBoard board;
 
+  private final int numberOfPlayers;
+
+  private final String map;
+
+  /*
+  public Risk(String yamlString) {
+    Yaml yaml = new Yaml();
+    Map<String, Object> config = yaml.load(yamlString);
+
+    int maxNumberOfPlayers = getMaximumNumberOfPlayers();
+    int numberOfJokers = 2;
+    int cardTypes = 3;
+    List<Integer[]> continents;
+  }*/
+
+  public Risk(Risk risk) {
+    this(risk.currentPlayerId, risk.canonical, risk.actionRecords, risk.board,
+        risk.numberOfPlayers, risk.map);
+  }
+
   public Risk(int currentPlayerId, boolean canonical,
-      List<ActionRecord<RiskAction>> actionRecords, RiskBoard board) {
+      List<ActionRecord<RiskAction>> actionRecords, RiskBoard board, int numberOfPlayers,
+      String map) {
     this.currentPlayerId = currentPlayerId;
     this.canonical = canonical;
     this.actionRecords = actionRecords;
     this.board = board;
+    this.numberOfPlayers = numberOfPlayers;
+    this.map = map;
   }
 
   @Override
@@ -27,12 +51,12 @@ public class Risk implements Game<RiskAction, RiskBoard> {
 
   @Override
   public int getMinimumNumberOfPlayers() {
-    return 0;
+    return 2;
   }
 
   @Override
   public int getMaximumNumberOfPlayers() {
-    return 0;
+    return Integer.MAX_VALUE;
   }
 
   @Override
@@ -88,5 +112,10 @@ public class Risk implements Game<RiskAction, RiskBoard> {
   @Override
   public Game<RiskAction, RiskBoard> getGame(int i) {
     return null;
+  }
+
+  @Override
+  public String toString() {
+    return "Risk: " + currentPlayerId + ", " + Arrays.toString(getGameUtilityValue());
   }
 }
